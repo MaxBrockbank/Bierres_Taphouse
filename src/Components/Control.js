ac98869939462c3a57efa76b545c4e548fe3d095
Max Brockbank <maxbrockbank1999@gmail.com>
2021-02-12 08:50:47 -0800

    build file structure with all planned components

---
+++ src/Components/Control.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import BeerList from './BeerList';
+
+class Control extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+
+    }
+  }
+
+  render(){
+    return();
+  }
+}
+
+export default Control;
